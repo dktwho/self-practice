64ff93106a57604671c3a285866e2c84af8fc882
Alexander Korobov <dktwho@mail.ru>
2023-08-09 20:54:17 +0300

    move props to apart variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+    const title = 'What to learn'
+    const title2 =100200
+    const title3 = true
+    const title22 = 'What to buy'
 
     return (
         <div className="App">
-            <Todolist title={'What tot learn'} title2={100200}/>
-            <Todolist title={'What tot buy'} title3={true} />
+            <Todolist title={title} title2={title2}/>
+            <Todolist title={title22} title3={title3} />
         </div>
     );
 }
