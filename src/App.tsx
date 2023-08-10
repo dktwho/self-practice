e5c1db7814dd28fbdd33047bbf066a485489f294
Alexander Korobov <dktwho@mail.ru>
2023-08-10 10:02:41 +0300

    remove one todolist

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Todolist} from "./Todolist";
 
 function App() {
     const title1 = 'What to learn'
-    const title2 = 'What to buy'
+
 
     const tasks1 = [
         {id: 1, title: 'HTML&CSS', isDone: true},
@@ -12,16 +12,12 @@ function App() {
         {id: 3, title: 'React', isDone: false},
     ]
 
-    const tasks2 = [
-        {id: 1, title: 'Buy Milk', isDone: true},
-        {id: 2, title: 'Buy water', isDone: false},
-        {id: 3, title: 'Buy Bread', isDone: false},
-    ]
+
 
     return (
         <div className="App">
             <Todolist title={title1} tasks={tasks1}/>
-            <Todolist title={title2} tasks={tasks2}/>
+
         </div>
     );
 }
