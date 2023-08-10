760881521372cfbaaf0bcb8e27532e311ce3bda8
Alexander Korobov <dktwho@mail.ru>
2023-08-10 10:45:38 +0300

    add useState, add logic for remove task, with setState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
-    const title1 = 'What to learn'
-
-    const tasks1 = [
+    const [tasks, setTasks] = useState([
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'Javascript', isDone: true},
         {id: 3, title: 'React', isDone: false},
-    ]
+    ])
+    const title1 = 'What to learn'
+
 
     const removeTask = (id: number) => {
-        console.log(id)
+        setTasks(tasks.filter(el => el.id !== id))
+
     }
 
 
     return (
         <div className="App">
-            <Todolist title={title1} tasks={tasks1} removeTask={removeTask}/>
+            <Todolist title={title1} tasks={tasks} removeTask={removeTask}/>
 
         </div>
     );
